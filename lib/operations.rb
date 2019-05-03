32980ce14f632dab827c4c803cfd1e9a9f75ba1b
Shivang Dave <shivang.dave@flatironschool.com>
2019-05-03 13:14:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+  #(speed > 60) ? true : (speed < 40 ? true : (speed > 40 && speed < 60 ? false : true))
+  if(speed>60)
+    true
+  else
+    if(speed < 40)
+      true
+    else
+      if(speed > 40 && speed < 60)
+        false
+      end
+    end
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+	(speed > 60) ? true : (speed < 40 ? true : (speed > 40 && speed < 60 ? false : true))
 end
 	
 
